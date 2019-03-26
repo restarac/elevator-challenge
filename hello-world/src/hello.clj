b0d0280d209b3c0d7777c2c741a39fc1862bbc64
Frederico Pereira <frederico.silva.pereira@gmail.com>
2019-03-25 21:36:04 -0300

    Learning somethings about the language

--- hello-world/src/hello.clj
+++ hello-world/src/hello.clj
@@ -2,6 +2,8 @@
   (:require [hello-time :as ht]))
 
 (defn -main []
-  (println "Hello World, the time is" (ht/time-str (ht/now)) "now with local libs" ))
+  (println "Hello World, the time is" (ht/time-str (ht/now)) "now with local libs" )
+  (prn 'Results '(+ 3 1) (+ 3 1) )
+  (prn "( 7 + 3 * 4 + 5 ) / 10 = " (/ (* (+ 3 1) (+ 4 5)) 10) ))
 
 ; runs `$ clj -m hello`
