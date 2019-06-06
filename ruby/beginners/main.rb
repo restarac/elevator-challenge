ac1d7fa5d8f072130bf707e75ba852781516cbf6
Frederico Pereira <frederico.pereira@dafiti.com.br>
2019-06-05 21:19:04 -0300

    rename the folder

--- ruby/beginners/main.rb
+++ ruby/beginners/main.rb
@@ -14,13 +14,38 @@ end
 
 class Employes
     attr_accessor :current, :final
+
     def initialize(current, final)
         @current = current
         @final_floor = final_floor
     end
 end
 
+class ElevatorPool
+    attr_reader :elevators
+
+    def initialize(elevators)
+        @elevators = elevators
+    end
+
+    def self.for(number)
+        elevators = []
+
+        for id in 1..number do
+            elevators << Elevator.new(id)
+        end
+
+        self.new(elevators)
+    end
+end
+
 class Elevator
+    attr_reader :id
+
+    def initialize(id)
+        @id = id
+    end
+
     def floor_by_minute
         5
     end
