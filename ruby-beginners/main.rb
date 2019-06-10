842b689eaeaad46e6176f91bf7c5caa9211755d0
Frederico Pereira <frederico.pereira@dafiti.com.br>
2019-06-10 20:00:27 -0300

    Continuing first steps on main, after that it will to separate in apropriate files and paths

--- ruby-beginners/main.rb
+++ ruby-beginners/main.rb
@@ -1,23 +1,48 @@
 class Building
-    attr_accessor :name, :floors
-    def initialize(name, floors)
+    attr_reader :name, :floors, :employees, :elevators
+
+    def initialize(name, floors, employees, elevators)
         @name = name
         @floors = floors
+        @employees = employees
+        @elevators = elevators
     end
 end
 
 class Floor
+    include Comparable
+    attr_reader :id
+
     def initialize(id)
-      @id = id
+        @id = id
+    end
+
+    def to_s
+        @id
+    end
+
+    def inspect
+        @id
+    end
+
+    def <=>(other)
+        @id <=> other.id
     end
 end
 
-class Employes
-    attr_accessor :current, :final
+class Employee
+    require 'securerandom'
+    attr_reader :name
+    attr_accessor :current, :objective
 
-    def initialize(current, final)
+    def initialize(current, objective, name = "Default")
         @current = current
-        @final_floor = final_floor
+        @objective = objective
+        @name = "#{name}-#{SecureRandom.uuid}"
+    end
+
+    def delivered?
+        @current == @objective
     end
 end
 
@@ -50,3 +75,29 @@ class Elevator
         5
     end
 end
+
+ground_floor = Floor.new("Terreo")
+
+puts "creating floors..."
+floors = [ground_floor]
+for id in 1..6 do
+    floors << Floor.new("floor #{id}")
+end
+puts "#{floors.size-1} floors created!"
+
+puts "creating employees..."
+employees = []
+for id in 1..150 do
+    employees << Employee.new(ground_floor, floors[5])
+end
+puts "#{employees.size} employees created!"
+
+puts "puting the employees on the entrance hall..."
+elevators = ElevatorPool.for(1)
+building = Building.new("Beginners Building", floors, employees, elevators)
+puts "The building is now complete!!!"
+
+require "awesome_print"
+ap building, options = {raw: false, limit: 10, class_name: :to_s}
+puts ground_floor
+
